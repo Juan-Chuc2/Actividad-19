ae44f8e23663d921faa205c9bfe9f0944e055e78
Juan Diego <chucjuan508@gmail.com>
2025-08-13 18:26:24 -0800

    4to commit registro de galleta rellena y clase de la misma

--- Actividad 19.py
+++ Actividad 19.py
@@ -7,10 +7,25 @@ class Galleta:
          return f"Nombre de la Galleta: {self.nombre}, Costo de la galleta: {self.precio}, Peso de la galleta: {self.peso}"
 class Galletachispas(Galleta):
     def __init__(self, nombre,precio,peso,cantidad_chispas):
-        super.__init__(nombre,precio,peso)
+        super().__init__(nombre,precio,peso)
         self.cantidad_chispas = cantidad_chispas
     def mostrar_info(self):
         return super().mostrar_informacion() + f",Cantidad de chispas {self.cantidad_chispas}"
+
+class Relleno:
+    def __init__(self, sabor_del_relleno):
+        self.sabor_del_relleno = sabor_del_relleno
+
+    def describir_relleno(self):
+        return f"El sabor del relleno: {self.sabor_del_relleno}"
+
+class GalletaconRelleno(Galleta, Relleno):
+    def __init__(self, nombre, precio, peso, sabor_relleno):
+        Galleta.__init__(self, nombre, precio, peso)
+        Relleno.__init__(self, sabor_relleno)
+
+    def mostrar_informacion(self):
+        return super().mostrar_informacion() + f", {self.describir_relleno()}"
 
 class Registrar_galleta:
     def __init__(self):
@@ -29,7 +44,18 @@ class Registrar_galleta:
             nombre = input("Ingrese el nombre de la galleta: ").strip()
             precio = float(input("Ingrese el precio de la galleta:  "))
             peso = float(input("Ingrese el peso de la Galleta: "))
-            self.registro_galletas.append(Galleta(nombre, precio, peso))
+            cantidad_chispas = int(input("Ingrese la cantidad de chispas: "))
+            self.registro_galletas.append(Galleta(nombre, precio, peso, cantidad_chispas))
+            print("Galleta basica Registrada ")
+        except ValueError:
+            print("Error al ingresar datos")
+    def add_galleta_rellena(self):
+        try:
+            nombre = input("Ingrese el nombre de la galleta: ").strip()
+            precio = float(input("Ingrese el precio de la galleta:  "))
+            peso = float(input("Ingrese el peso de la Galleta: "))
+            sabor = input("Ingrese el baro de la galleta: ")
+            self.registro_galletas.append(GalletaconRelleno(nombre, precio, peso, sabor))
             print("Galleta basica Registrada ")
         except ValueError:
             print("Error al ingresar datos")
